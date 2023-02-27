492ce293846bf86ca299d11b3545c7b88a7b5530
Arkadiy <alibullterrier@gmail.com>
2023-02-27 21:30:50 +0200

    refactoring

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -11,18 +11,19 @@ type PropsType = {
 type TasksType = {
     id: Number, 
     title: String, 
-    isDone: Boolean
+    isDone: boolean
 }
 
-{props.tasks.map((el) => {
-    return  <li>
-        <input type="checkbox" checked={el.isDone}/> 
-        <span>{el.title}</span>
-        </li>
 
-})}
 
 const Todo = (props: PropsType) => {
+    const tasksMap = props.tasks.map((el) => {
+        return(  <li>
+            <input type="checkbox" checked={el.isDone}/> 
+            <span>{el.title}</span>
+            </li>
+    )})
+
     return(
         <div>
                 <h3>{props.headTitle}</h3>
@@ -33,9 +34,11 @@ const Todo = (props: PropsType) => {
                     <button>+</button>
                 </div>
                 <ul>
-                    <li><input type="checkbox" checked={true}/> <span>{props.tasks[0].title}</span></li>
+                    {tasksMap}
+                    {/*<li><input type="checkbox" checked={true}/> <span>{props.tasks[0].title}</span></li>
                     <li><input type="checkbox" checked={true}/> <span>{props.tasks[1].title}</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>{props.tasks[2].title}</span></li>
+    <li><input type="checkbox" checked={false}/> <span>{props.tasks[2].title}</span></li>*/}
+
                 </ul>
                 <div>
                     <button>All</button>
