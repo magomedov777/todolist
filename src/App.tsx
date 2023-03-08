8753a35e5bb1eab018cb057f5084b385195209a3
Arkadiy <alibullterrier@gmail.com>
2023-03-08 10:06:38 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Todolist from './Todolist';
 const App = () => {
 let[tasks1, setTasks] = useState([
         { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
+        { id: 2, title: "JS", isDone: true },                     //передаем данные тасок в хук useState 
         { id: 3, title: "ReactJS", isDone: false }
     ])
 
